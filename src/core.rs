289c2662aff982a611b08d637561f389b5f5ec80
Andrew Fryer <andrew.fryer@queensu.ca>
2023-01-31 12:35:46 -0500

    more core trait stuff

--- src/core.rs
+++ src/core.rs
@@ -1,6 +1,14 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, slice::Iter};
 
-pub trait DataModel: Parser + Ast + Fuzzer + Serializer {}
+pub trait DataModel: Breed + Cloneable + Parser + Ast + Fuzzer + Vectorizer + Serializer {}
+
+pub trait Cloneable {
+    fn clone(&self) -> dyn DataModel;
+}
+
+pub trait Breed {
+    fn breed(&self, other: dyn DataModel) -> dyn DataModel;
+}
 
 pub trait Parser {
     fn parse(&self, input: BinaryStream, ctx: Context) -> dyn DataModel; // why don't I need a box around `dyn DataModel`?
@@ -10,6 +18,49 @@ pub trait Ast {
 }
 pub trait Fuzzer {
     fn fuzz(&self) -> Vec<Box<dyn DataModel>>;
+}
+
+pub struct FeatureVector {
+    fs: Vec<String>,
+    d: HashMap<String, f64>,
+}
+impl FeatureVector {
+    pub fn new(features: Vec<String>) -> Self {
+        let mut d = HashMap::new();
+        for f in &features {
+            d.insert(f.to_string(), 0f64); // todo: avoid cloning String?
+        }
+        Self {
+            fs: features,
+            d,
+        }
+    }
+    pub fn tally(&mut self, feature: String, depth: i32) {
+        *self.d.get_mut(&feature).unwrap() += std::f64::consts::E.powi(depth);
+    }
+    pub fn values(&self) -> Vec<f64> {
+        let mut result = Vec::new();
+        for f in self.fs.iter() {
+            result.push(*self.d.get(&f.to_string()).unwrap());
+        }
+        result
+    }
+    pub fn dist(&self, other: &FeatureVector) -> f64 {
+        assert!(self.features().collect::<Vec<&String>>() == other.features().collect::<Vec<&String>>());
+        let mut result = 0f64;
+        for (self_val, other_val) in self.values().iter().zip(other.values().iter()){
+            result += (self_val - other_val).powi(2);
+        }
+        result
+    }
+    pub fn features(&self) -> Iter<'_, String> {
+        self.fs.iter()
+    }
+}
+// impl Iterator for FeatureVector
+pub trait Vectorizer {
+    fn features(&self) -> FeatureVector;
+    fn vectorize(&self) -> FeatureVector;
 }
 pub trait Serializer {
     fn serialize(&self) -> BinaryStream;
