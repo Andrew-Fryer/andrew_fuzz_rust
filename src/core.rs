7041c33e63e2b5ca6567623c3662e940810a5a87
Andrew Fryer <andrew.fryer@queensu.ca>
2023-02-01 10:23:20 -0500

    basic bitarray implementation with todos

--- src/core.rs
+++ src/core.rs
@@ -1,5 +1,5 @@
 use core::num;
-use std::{collections::HashMap, slice::Iter};
+use std::{collections::HashMap, slice::Iter, rc::Rc, cell::RefCell};
 
 mod feature_vector;
 use feature_vector::FeatureVector;
@@ -34,33 +34,72 @@ pub trait Serializer {
 
 // I have cool ideas about how to optimize this!
 // we could make it an enum that could be normal or could be a Vec<Box<BitArray>> which is sort of lazy...
-pub struct BitArray<'a> {
-    data: &'a Vec<u8>,
-    offset: i32,
+// pub enum BitArray<'a> {
+//     Ref {
+//         data: &'a Vec<u8>,
+//         offset: i32,
+//         len: i32,
+//     },
+// }
+
+// BitArray does bit stuff for us (on the underlying bytes).
+pub struct BitArray {
+    data: Rc<RefCell<Vec<u8>>>,
+    pos: i32,
     len: i32,
 }
-impl BitArray<'_> {
+impl BitArray {
     pub fn new(num_bits: i32) -> Self {
         Self {
-            data: Vec::with_capacity((num_bits / 8) as usize),
-            offset: 0,
+            data: Rc::new(RefCell::new(Vec::with_capacity((num_bits / 8) as usize))),
+            pos: 0,
             len: num_bits,
         }
     }
     pub fn clone(&self) -> Self {
         Self {
-            data: self.data,
-            offset: self.offset,
+            data: self.data.clone(),
+            pos: self.pos,
             len: self.len,
         }
     }
-    pub fn advance(&mut self, num_bits: i32) {
-        self.offset += num_bits;
+    pub fn chunks(&mut self) -> &Vec<u8> {
+        &self.data.borrow() // todo: make this start at pos and go until len
     }
-    // pub fn extend(&mut self, other: &BitArray) {
-    //     self.data.extend_from_slice(other.asdf());
-    //     self.len += other.len; //
-    // }
+    pub fn len(&self) -> i32 {
+        self.len
+    }
+    pub fn eat(&mut self, num_bits: i32) -> Self {
+        // This is efficient because the only mutation we do to a BitArray is extending it.
+        // So, to `eat`, we can share the underlying `Vec<u8>`.
+        let next_pos = self.pos + num_bits;
+
+        let result = if num_bits % 8 == 0 {
+            if num_bits % 8 == 0 {
+                Self {
+                    // data: (&self.data[(self.pos)..(self.pos + num_bits)]).into(),
+                    data: self.data,
+                    pos: self.pos,
+                    len: num_bits,
+                }
+            } else {
+                todo!();
+            }
+        } else {
+            todo!();
+        };
+
+        self.pos += num_bits;
+
+        result
+    }
+    pub fn advance(&mut self, num_bits: i32) {
+        self.pos += num_bits;
+    }
+    pub fn extend(&mut self, other: &BitArray) {
+        self.data.borrow_mut().extend_from_slice(other.chunks());
+        self.len += other.len();
+    }
     // pub fn get(&self) -> 
 }
 
