44bd0ba3c0d78bae2416326337bce8fb3fddc660
Andrew Fryer <andrew.fryer@queensu.ca>
2023-02-01 10:35:50 -0500

    mv bit_array

--- src/core.rs
+++ src/core.rs
@@ -1,6 +1,8 @@
 use core::num;
-use std::{collections::HashMap, slice::Iter, rc::Rc, cell::RefCell};
+use std::{collections::HashMap, slice::Iter};
 
+mod bit_array;
+use bit_array::BitArray;
 mod feature_vector;
 use feature_vector::FeatureVector;
 
@@ -30,77 +32,6 @@ pub trait Vectorizer {
 }
 pub trait Serializer {
     fn serialize(&self) -> BitArray;
-}
-
-// I have cool ideas about how to optimize this!
-// we could make it an enum that could be normal or could be a Vec<Box<BitArray>> which is sort of lazy...
-// pub enum BitArray<'a> {
-//     Ref {
-//         data: &'a Vec<u8>,
-//         offset: i32,
-//         len: i32,
-//     },
-// }
-
-// BitArray does bit stuff for us (on the underlying bytes).
-pub struct BitArray {
-    data: Rc<RefCell<Vec<u8>>>,
-    pos: i32,
-    len: i32,
-}
-impl BitArray {
-    pub fn new(num_bits: i32) -> Self {
-        Self {
-            data: Rc::new(RefCell::new(Vec::with_capacity((num_bits / 8) as usize))),
-            pos: 0,
-            len: num_bits,
-        }
-    }
-    pub fn clone(&self) -> Self {
-        Self {
-            data: self.data.clone(),
-            pos: self.pos,
-            len: self.len,
-        }
-    }
-    pub fn chunks(&mut self) -> &Vec<u8> {
-        &self.data.borrow() // todo: make this start at pos and go until len
-    }
-    pub fn len(&self) -> i32 {
-        self.len
-    }
-    pub fn eat(&mut self, num_bits: i32) -> Self {
-        // This is efficient because the only mutation we do to a BitArray is extending it.
-        // So, to `eat`, we can share the underlying `Vec<u8>`.
-        let next_pos = self.pos + num_bits;
-
-        let result = if num_bits % 8 == 0 {
-            if num_bits % 8 == 0 {
-                Self {
-                    // data: (&self.data[(self.pos)..(self.pos + num_bits)]).into(),
-                    data: self.data,
-                    pos: self.pos,
-                    len: num_bits,
-                }
-            } else {
-                todo!();
-            }
-        } else {
-            todo!();
-        };
-
-        self.pos += num_bits;
-
-        result
-    }
-    pub fn advance(&mut self, num_bits: i32) {
-        self.pos += num_bits;
-    }
-    pub fn extend(&mut self, other: &BitArray) {
-        self.data.borrow_mut().extend_from_slice(other.chunks());
-        self.len += other.len();
-    }
-    // pub fn get(&self) -> 
 }
 
 pub struct BinaryStream {
