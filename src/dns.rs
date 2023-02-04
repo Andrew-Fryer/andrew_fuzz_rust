e69d3caa59e4dca371a7f0e738bf4235f9458113
Andrew Fryer <andrew.fryer@queensu.ca>
2023-02-04 11:34:04 -0500

    trying to get dns grammar to compile

--- src/dns.rs
+++ src/dns.rs
@@ -3,19 +3,20 @@ use std::{collections::HashMap, rc::Rc};
 use crate::{library::{set::Set, sequence::Sequence, u8::U8, u16::U16, button::Button}, core::DataModel};
 
 pub fn dns() -> Box<dyn DataModel> {
+    let u16 = Rc::new(U16::new());
     let resource_record = todo!();
-    // Box::new(Sequence::new(HashMap::from([
-    //     ("transactionId", U16::new()),
-    //     ("flags", U16::new()),
-    //     ("numQuestion", U16::new()),
-    //     ("numAnswer", U16::new()),
-    //     ("numAuthority", U16::new()),
-    //     ("numAdditional", U16::new()),
-    //     ("question", Set::new(resource_record, Vec::new(), Rc::new(|ctx| ctx.vec().len() as i32 == ctx.parent().map()["numQuestion"].int()))),
-    //     ("answer", Set::new(resource_record, Vec::new(), Rc::new(|ctx| ctx.vec().len() as i32 == ctx.parent().map()["numAnswer"].int()))),
-    //     ("authority", Set::new(resource_record, Vec::new(), Rc::new(|ctx| ctx.vec().len() as i32 == ctx.parent().map()["numAuthority"].int()))),
-    //     ("additional", Set::new(resource_record, Vec::new(), Rc::new(|ctx| ctx.vec().len() as i32 == ctx.parent().map()["numAdditional"].int()))),
-    //     ("end", Button::new()),
-    // ])));
-    todo!()
+    let result = Box::new(Sequence::new(HashMap::from([
+        // ("transactionId".to_string(), u16.clone()),
+        // ("flags".to_string(), u16.clone()),
+        // ("numQuestion".to_string(), u16.clone()),
+        // ("numAnswer".to_string(), u16.clone()),
+        // ("numAuthority".to_string(), u16.clone()),
+        // ("numAdditional".to_string(), u16.clone()),
+        // ("question".to_string(), Rc::new(Set::new(resource_record, Vec::new(), Rc::new(|ctx| ctx.vec().len() as i32 == ctx.parent().map()["numQuestion".to_string()].int())))),
+        // ("answer".to_string(), Rc::new(Set::new(resource_record, Vec::new(), Rc::new(|ctx| ctx.vec().len() as i32 == ctx.parent().map()["numAnswer".to_string()].int())))),
+        // ("authority".to_string(), Rc::new(Set::new(resource_record, Vec::new(), Rc::new(|ctx| ctx.vec().len() as i32 == ctx.parent().map()["numAuthority".to_string()].int())))),
+        // ("additional".to_string(), Rc::new(Set::new(resource_record, Vec::new(), Rc::new(|ctx| ctx.vec().len() as i32 == ctx.parent().map()["numAdditional".to_string()].int())))),
+        // ("end".to_string(), Button::new()),
+    ])));
+    result
 }
