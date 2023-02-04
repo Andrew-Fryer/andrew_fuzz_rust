525a8edd783a6e275505e93a18bf38c16c53a379
Andrew Fryer <andrew.fryer@queensu.ca>
2023-02-04 11:38:05 -0500

    ...

--- src/dns.rs
+++ src/dns.rs
@@ -3,10 +3,10 @@ use std::{collections::HashMap, rc::Rc};
 use crate::{library::{set::Set, sequence::Sequence, u8::U8, u16::U16, button::Button}, core::DataModel};
 
 pub fn dns() -> Box<dyn DataModel> {
-    let u16 = Rc::new(U16::new());
+    let u16: Rc<dyn DataModel> = Rc::new(U16::new());
     let resource_record = todo!();
     let result = Box::new(Sequence::new(HashMap::from([
-        // ("transactionId".to_string(), u16.clone()),
+        ("transactionId".to_string(), u16.clone()),
         // ("flags".to_string(), u16.clone()),
         // ("numQuestion".to_string(), u16.clone()),
         // ("numAnswer".to_string(), u16.clone()),
